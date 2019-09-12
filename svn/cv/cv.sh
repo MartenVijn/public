41fbd48ad2d012b1d59baeac91ec9fa615ccd240
Marten <info@martenvijn.nl>
2019-09-12 23:04:08 +0200

    updating cv

--- svn/cv/cv.sh
+++ svn/cv/cv.sh
@@ -3,14 +3,10 @@
 # npm install markdown-pdf -g
 # npm install markdown -g
 
-for file in `ls *.md | cut -d'.' -f1`
+for file in cv_2019
 do	
 #	md2html -f $file.md 
 	md2html $file.md > ${file}.html
-    markdown-pdf $file.md
-	svn add *
-	svn propset svn:mime-type application/pdf $file.pdf
-        svn propset svn:mime-type text/html $file.html
+        markdown-pdf $file.md
 done
 
-svn ci -m "update $1"
